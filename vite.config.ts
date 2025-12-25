5659756147223e1f26c14db19dea679ec9da49f0
yaminsoong <ahmad.askhabulyamin11@gmail.com>
2025-12-25 17:04:54 +0000

    up

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/askha-anisa/',
       server: {
         port: 3000,
         host: '0.0.0.0',
